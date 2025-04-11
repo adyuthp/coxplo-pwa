a91298a5da055a99b8571a5edec6f28dc8eb070a
Adyuth <adyu@Adyus-MacBook-Pro.local>
2025-04-11 15:42:29 +0530

    pwa

--- service-worker.js
+++ service-worker.js
@@ -1,57 +1,43 @@
-
-const CACHE_NAME = 'coxplo-cache-v2';
-const OFFLINE_URL = 'offline.html';
-
+const CACHE_NAME = 'coxplo-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/offline.html',
-  '/manifest.json',
   '/assets/css/style.css',
-  '/assets/js/custom.js'
+  '/assets/css/responsive.css',
+  '/assets/js/custom.js',
+  '/assets/img/logo.png',
+  '/assets/img/heading-img-2.ico',
+  // Add other assets you want to cache
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
       return cache.addAll(urlsToCache);
     })
   );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match(OFFLINE_URL))
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request).then((res) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request.url, res.clone());
-            return res;
-          });
-        });
-      }).catch(() => {
-        return caches.match(OFFLINE_URL);
-      })
-    );
-  }
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
